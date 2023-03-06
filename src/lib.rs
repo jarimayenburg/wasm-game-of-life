c6fcb7401690f111834f0fe7928e327c0142a2b8
Jari Maijenburg <jari.maijenburg@formelio.nl>
2023-03-06 04:39:16 +0100

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,19 @@ impl Universe {
     }
 }
 
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+}
+
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
@@ -81,8 +94,24 @@ impl Universe {
         self.width
     }
 
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_| Cell::Dead).collect();
+    }
+
     pub fn height(&self) -> u32 {
         self.height
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_| Cell::Dead).collect();
     }
 
     pub fn cells(&self) -> *const Cell {
